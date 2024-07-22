d4d9e7d631f4c4d43418b0d052314b0177cec384
lal123 <lalhriemsangfaihriemsang@gmail,com>
2024-07-22 12:53:12 +0530

    commented dotenv

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,5 @@
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 import { MongoClient } from "mongodb";
 // const defaultURL = "mongodb://localhost:27017/Christians-IITG-Books";
 //  "mongodb+srv://lalhriemsang:lal%40123%23777@cluster0.7ik7jus.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
