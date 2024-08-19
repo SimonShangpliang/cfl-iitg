9d5e1e6b0fc146a96aa7c3f3d6ea1cc1f5be0981
lal123 <lalhriemsangfaihriemsang@gmail,com>
2024-08-19 19:12:03 +0530

    test

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -91,7 +91,7 @@ async function sendEmail(bookName, recipientMail) {
   }
 }
 // Schedule the task to run every day at midnight
-cron.schedule("1 * * * *", async () => {
+cron.schedule("* * * * *", async () => {
   console.log("Running cron job to check due dates and send emails...");
   try {
     await checkDueDatesAndSendEmails();
